bef47ce9586599e25224e5ba2c5099f03410e6bf
ChaMyungjun <acg70159@gmail.com>
2021-10-28 21:47:02 +0900

    create container style guide

---
+++ components/Container/index.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 980px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: auto;
+  padding: 15px;
+`;
