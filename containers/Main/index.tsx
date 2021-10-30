63cfde13c1d2f87fceeec3e7fd5a8108f14278e4
ChaMyungjun <acg70159@gmail.com>
2021-10-30 17:29:14 +0900

    Main page update

--- containers/Main/index.tsx
+++ containers/Main/index.tsx
@@ -4,16 +4,24 @@ import styled from "styled-components";
 //components
 // import Header from "components/Header"
 import { Container } from "components/Container";
+import { Title } from "components/Typography";
+
+//config
 import { prefix } from "config/config";
 
 const MainContainer = () => {
   return (
     <React.Fragment>
-      <BackgroundImg
-        src={`${prefix}/images/Main/MainTop.png`}
-        style={{ width: `100%`, height: 500 }}
-      />
-      <Container></Container>
+      <Container>
+        <BackgroundImg src={`${prefix}/images/Main/MainTop.png`}>
+          <TextWrap>
+            <CustomTitle>Use Next.js or Nest.js your code</CustomTitle>
+            <CustomTitle>
+              Use this library then recommend this website
+            </CustomTitle>
+          </TextWrap>
+        </BackgroundImg>
+      </Container>
     </React.Fragment>
   );
 };
@@ -25,6 +33,18 @@ const BackgroundImg = styled.div<{ src: string }>`
   height: 450px;
   background: url(${(props) => props.src});
   background-repeat: no-repeat;
-  background-size: cover;
+  background-size: contain;
   background-position: center center;
+`;
+
+const TextWrap = styled.div`
+  width: 100%;
+  margin: 100px auto;
+  text-align: center;
+`;
+
+const CustomTitle = styled(Title)`
+  font-weight: 700;
+  font-size: 50px;
+  white-space: pre;
 `;
