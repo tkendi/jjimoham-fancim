6e689317f7e78049883bc756f600f777af627a3b
ChaMyungjun <acg70159@gmail.com>
2022-05-01 22:52:17 +0900

    update: update change next.config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
-// /** @type {import('next').NextConfig} */
-// module.exports = {
-//   reactStrictMode: true,
-// }
-
-const debug = process.env.NODE_ENV !== "production";
-const name = "small-magic-project-deployment";
-
 module.exports = {
-  assetPrefix:
-    process.env.NODE_ENV === "production"
-      ? "https://chamyungjun.github.io/tkendi-blog/"
-      : "",
+  poweredByHeader: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
-
-// module.exports = {
-//   images: {
-//     loader: (width, src, quality) =>
-//       `/preprocessed-images/${filename}-${width}.jpg`,
-//   },
-// };
