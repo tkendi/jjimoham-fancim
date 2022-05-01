c9e17aa231a223aa240bfbd35ebda8bcf8be6e27
ChaMyungjun <acg70159@gmail.com>
2022-05-01 23:09:02 +0900

    upate: update document meta tag setting

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,11 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
-import { ServerStyleSheet } from "styled-components";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+const SITE_NAME = "찌모햄"
+const SITE_URL = "https://jjimoham.vercel.app/"
+const SITE_DESCRIPTION = "찌모햄 팬심"
+const SITE_TITLE = "햄떡햄떡"
+const SITE_IMAGE = "/images/og/OG.png"
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: any) {
@@ -32,6 +38,47 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="apple-touch-icon" href="/icons/120.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/icons/152.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/icons/180.png" />
+          <link rel="apple-touch-icon" sizes="167x167" href="/icons/167.png" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+
+          <meta name="theme-color" content="#ffffff" />
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta httpEquiv="Content-Type" content="text/xml; charset=utf-8" />
+
+          <link rel="canonical" href={SITE_URL} />
+          <meta name="description" content={SITE_DESCRIPTION} />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content={SITE_URL} />
+          <meta property="og:site_name" content={SITE_NAME} />
+          <meta property="og:title" content={SITE_TITLE} />
+          <meta property="og:description" content={SITE_DESCRIPTION} />
+          <meta property="og:image" content={SITE_IMAGE} />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content={SITE_URL} />
+          <meta name="twitter:site" content={SITE_NAME} />
+          <meta name="twitter:title" content={SITE_TITLE} />
+          <meta name="twitter:description" content={SITE_DESCRIPTION} />
+          <meta property="twitter:image" content={SITE_IMAGE} />
+
+          <meta
+            name="format-detection"
+            content="telephone=no, address=no, email=no"
+          />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+          />
+
+          <link rel="shortcut icon" href="/favicon.ico" />
+
           {/* google fonts */}
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
