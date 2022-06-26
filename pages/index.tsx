8309e0c6e146f8ad62cb72c6ae986d643863b245
ChaMyungjun <acg70159@gmail.com>
2022-06-26 21:40:16 +0900

    [FEAT] Feat graphql setting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,8 @@
 import type { NextPage } from 'next';
+import { gql } from 'apollo-boost';
+
+//api
+import client from 'api/GRAPHQL';
 
 //container
 import MainContainer from 'containers/Main';
@@ -6,7 +10,9 @@ import MainContainer from 'containers/Main';
 //components
 import Layout from 'components/Layout';
 
-const Home: NextPage = () => {
+const Home: NextPage = (data) => {
+  console.log('testing', data ? true : false);
+
   return (
     <Layout noHeader>
       <MainContainer />
@@ -15,3 +21,28 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getStaticProps = async () => {
+  // apollo connection graphql query, and get data
+
+  const { data } = await client.query({
+    query: gql`
+      query {
+        getAllNotification {
+          id
+          name
+          email
+          number
+        }
+      }
+    `,
+  });
+
+  // return data props: {data}
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
